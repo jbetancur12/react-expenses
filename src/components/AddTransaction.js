87bb2ead2d42a50cfeb54317f5da5d8880203c5d
Jorge Betancur <jbetancur@elespectador.com>
2020-11-17 01:56:59 +0000

    UI end

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,55 @@
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id:
+        Math.round(new Date().getTime() / 1000)
+          .toString(36)
+          .substr(2, 9) +
+        '_' +
+        Math.random().toString(36).substr(2, 9),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+  };
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className='form-control'>
+          <label htmlFor='text'>Text</label>
+          <input
+            type='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder='Enter text...'
+          />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='amount'>
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type='number'
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder='Enter amount...'
+          />
+        </div>
+        <button className='btn'>Add transaction</button>
+      </form>
+    </>
+  );
+};
